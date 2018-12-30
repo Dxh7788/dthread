3f67087e937b5d000f777d6ba34e6dea42f50933
dongxiaohong <dongxiaohong@daokoudai.com>
2018-12-30 20:49:57 +0800

    init java

---
+++ src/main/java/com/dthread/App.java
@@ -0,0 +1,13 @@
+package com.dthread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
