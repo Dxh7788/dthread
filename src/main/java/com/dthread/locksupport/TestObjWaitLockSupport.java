baca140ea89d4b6efe86dee24779b511eafc196f
dongxiaohong <dongxiaohong@daokoudai.com>
2019-01-17 09:52:23 +0800

    LockSupport 测试

--- src/main/java/com/dthread/locksupport/TestObjWaitLockSupport.java
+++ src/main/java/com/dthread/locksupport/TestObjWaitLockSupport.java
@@ -9,20 +9,25 @@ public class TestObjWaitLockSupport {
 
     public static void main(String[] args)throws Exception {
         final Object obj = new Object();
-        Thread A = new Thread(()-> {
-            int sum = 0;
-            for(int i=0;i<10;i++){
-                sum+=i;
-            }
-            LockSupport.park();
-            System.out.println(sum);
-        });
-        A.start();
-        LockSupport.unpark(A);
+        TestObjWaitLockSupport support = new TestObjWaitLockSupport();
+        support.exec();
         //睡眠一秒钟，保证线程A已经计算完成，阻塞在wait方法
         //Thread.sleep(1000);
         /**
          * 有可能下面语句先执行,导致线程A阻塞
          * */
+    }
+
+    public void exec() {
+        Thread A = new Thread(()-> {
+            int sum = 0;
+            for(int i=0;i<10;i++){
+                sum+=i;
+            }
+            LockSupport.park(this);
+            System.out.println(sum);
+        });
+        A.start();
+        LockSupport.unpark(A);
     }
 }
